5df085dd9b30feded6837544dfacb15fbd3f9a97
home <andreagrippa76@gmail.com>
2020-11-20 17:04:31 -0500

    Made Vector2D class

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -1,8 +1,16 @@
 package ca.durhamcollege;
+import java.util.Scanner;
 
 public class Main {
 
+    public Main()
+    {
+        Vector2D point1;
+        Scanner scanner ;
+        System.out.println("Enter the first point (x,y):");
+    }
+
     public static void main(String[] args) {
-	// write your code here
+	new Main();
     }
 }
